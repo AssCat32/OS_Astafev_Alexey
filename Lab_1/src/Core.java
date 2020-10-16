a34522c4d913dcfbf72d2a139936110c88b50e6a
alexey <astafev1502276@bk.ru>
2020-10-16 15:09:00 +0400

    Лабораторная #1 Сменил HashMap на ArrayList

--- Lab_1/src/Core.java
+++ Lab_1/src/Core.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class Core {
-    private final int sytemCallsCount = 5;
-    private HashMap<Integer, SystemCall> patterns;
+    private final int systemCallsCount = 5;
+    private ArrayList<SystemCall> patterns;
     private Stack stack;
     private SystemCall call;
 
 
     public Core(Stack stack1) {
-        patterns = new HashMap<>();
-        patterns.put(0, new SystemCall(0, new Object[]{1, 1}));
-        patterns.put(1, new SystemCall(1, new Object[]{"string", 1.5, "string"}));
-        patterns.put(2, new SystemCall(2, new Object[]{"string", 1}));
-        patterns.put(3, new SystemCall(3, new Object[]{"string", "string"}));
-        patterns.put(4, new SystemCall(4, new Object[]{1, 1.5, "string"}));
+        patterns = new ArrayList<>();
+        patterns.add(0, new SystemCall(0, new Object[]{1, 1}));
+        patterns.add(1, new SystemCall(1, new Object[]{"string", 1.5, "string"}));
+        patterns.add(2, new SystemCall(2, new Object[]{"string", 1}));
+        patterns.add(3, new SystemCall(3, new Object[]{"string", "string"}));
+        patterns.add(4, new SystemCall(4, new Object[]{1, 1.5, "string"}));
         stack = stack1;
     }
 
 
     public void printSystemCalls() {
-        for (int i = 0; i < sytemCallsCount; i++) {
+        for (int i = 0; i < systemCallsCount; i++) {
             StringBuilder str = new StringBuilder("");
             str.append("Id: "+i+" Args: ");
             for (int j = 0; j < patterns.get(i).getArgs().length; j++) {
@@ -32,13 +33,18 @@ public class Core {
     }
 
     public void performSystemCall(int id) {
-        call = patterns.get(id);
+        try{
+            call = patterns.get(id);
+        }
+        catch (Exception e){
+            System.out.println("Error: Системного вызова с таким id не существует!");
+        }
 
         if (stack.getSize() == 0) {
             System.out.println("Стэк пуст");
             return;
         }
-        if (!patterns.containsKey(id)) {
+        if (!patterns.contains(call)) {
             System.out.println("Error: Системного вызова с таким id нет");
             return;
         }
