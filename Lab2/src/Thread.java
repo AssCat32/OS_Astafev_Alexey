32d078c370f9da74c74d9142d1962075fd304910
alexey <astafev1502276@bk.ru>
2020-10-26 12:24:29 +0400

    Лабораторная #2

---
+++ Lab2/src/Thread.java
@@ -0,0 +1,26 @@
+public class Thread {
+    private int tID;
+    private int necessaryTime;
+
+    public Thread (int tID,int necessaryTime){
+        this.tID=tID;
+        this.necessaryTime=necessaryTime;
+    }
+
+    public int getID() {
+        return tID;
+    }
+
+    public int getNecessaryTime() {
+        return necessaryTime;
+    }
+
+    public void changeNecessaryTime(int time){
+        necessaryTime-=time;
+    }
+
+    public void threadDone() {
+        System.out.println("Поток "+ tID + " выполнился успешно за время "+ necessaryTime);
+    }
+}
+
