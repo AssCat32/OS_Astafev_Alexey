f9eaa55a0989c525e884f8b0c6ae6f859eb0a5ec
Alexey Astafev <31274867+AssCat32@users.noreply.github.com>
2020-12-19 13:40:37 +0400

    Update Cell.java

--- OS_lab_4/src/Cell.java
+++ OS_lab_4/src/Cell.java
@@ -2,8 +2,6 @@ public class Cell {
 
     private int cellStatus;
 
-    private Cell nextCell;
-
     public Cell(int cellStatus) {
         this.cellStatus = cellStatus;
     }
@@ -14,13 +12,5 @@ public class Cell {
 
     public void setCellStatus(int cellStatus) {
         this.cellStatus = cellStatus;
-    }
-
-    public Cell getNextCell() {
-        return nextCell;
-    }
-
-    public void setNextCell(Cell nextCell) {
-        this.nextCell = nextCell;
     }
 }
